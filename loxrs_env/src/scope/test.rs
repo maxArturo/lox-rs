2bcc54d6d51a146756a3a338a37aa33545b5223a
Max Alcala <maxarturo@gmail.com>
2024-05-26 07:49:55 -0300

    fix: tests

--- loxrs_env/src/scope/test.rs
+++ loxrs_env/src/scope/test.rs
@@ -21,21 +21,10 @@ fn basics() {
 #[test]
 fn scope_assignment() {
     let scope = Rc::new(Scope::default());
-    let child = Scope::from_parent(Rc::clone(&scope));
 
     assert!(scope.get("foo").is_err());
     scope.define("foo", "bar".to_owned());
     assert!(scope.get("foo").is_ok_and(|str| str == "bar"));
-    assert!(child.get("foo").is_ok_and(|str| str == "bar"));
-
-    child.define("baz", "fff".to_owned());
-    assert!(child.get("baz").is_ok_and(|str| str == "fff"));
-
-    assert!(scope.get("baz").is_err());
-    assert!(scope.assign("baz", "bar".to_owned()).is_err());
-
-    assert!(child.assign("baz", "bar".to_owned()).is_ok());
-    assert!(child.get("baz").is_ok());
 }
 
 #[test]
@@ -50,18 +39,13 @@ fn ancestry() {
 
     child.define("child", true);
 
-    // assert!(child.ancestor(0).is_some_and(|el| { el.eq(&child) }));
+    assert!(child.get_at(0, "child").is_ok_and(|el| el));
+    assert!(child.get_at(1, "parent").is_ok_and(|el| el));
+    assert!(child.get_at(2, "grandy").is_ok_and(|el| el));
 
-    // grandparent.define("foo", "bar".to_owned());
-    // assert!(grandparent.get("foo").is_ok_and(|str| str == "bar"));
-    // assert!(parent.get("foo").is_ok_and(|str| str == "bar"));
-    //
-    // parent.define("baz", "fff".to_owned());
-    // assert!(parent.get("baz").is_ok_and(|str| str == "fff"));
-    //
-    // assert!(grandparent.get("baz").is_err());
-    // assert!(grandparent.assign("baz", "bar".to_owned()).is_err());
-    //
-    // assert!(parent.assign("baz", "bar".to_owned()).is_ok());
-    // assert!(parent.get("baz").is_ok());
+    assert!(grandparent.assign_at(33, "foo", true).is_err());
+    assert!(grandparent.assign_at(1, "bar", true).is_err());
+    assert!(grandparent.assign_at(0, "works", true).is_ok());
+
+    assert!(child.get_at(2, "works").is_ok_and(|el| el));
 }
